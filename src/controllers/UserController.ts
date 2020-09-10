c6f0c411be3561c9253ae52f5a18da5529804b3e
Vitor de Souza <vitordesolza@gmail.com>
2020-09-10 13:55:01 -0300

    Melhorando o código

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,23 +1,29 @@
-import { Request, Response} from 'express';
+import { Request, Response } from 'express';
 import EmailService from '../services/EmailService';
 
 const users = [
-    {name: "teste", email: "teste@teste.com"},
-    {name: "teste2", email: "teste2@teste.com"},
-    {name: "teste3", email: "teste3@teste.com"}
+    { name: "teste", email: "teste@teste.com" },
+    { name: "teste2", email: "teste2@teste.com" },
+    { name: "teste3", email: "teste3@teste.com" }
 ]
 
 export default {
-    async index(req: Request, res: Response){
+    async index(req: Request, res: Response) {
         return res.json(users);
     },
 
-    async create(req: Request, res: Response){
+    async create(req: Request, res: Response) {
         const emailService = new EmailService();
-        emailService.sendMail(
-            { name: "Teste", email: "teste@teste.com" },
-            { subject: "Isso é um teste", body: "Aqui também é um teste" }
-        );
+        emailService.sendMail({
+            to: { 
+                name: "Teste", 
+                email: "teste@teste.com" 
+            },
+            message: { 
+                subject: "Isso é um teste", 
+                body: "Aqui também é um teste" 
+            }
+        });
 
         return res.send();
     }
