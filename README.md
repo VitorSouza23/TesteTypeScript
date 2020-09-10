93b1a4b877ad9c0f11a6f1e9cd5f1215e46e960b
Vitor de Souza <vitordesolza@gmail.com>
2020-09-10 13:35:29 -0300

    Adição de simulador de serviço de e-mail

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ npm install
 ### Para subir o servidor:
 npm run dev:server
 
+Segui esse tutorial no YouTube: https://www.youtube.com/watch?v=0mYq5LrQN1s&ab_channel=Rocketseat
+
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response} from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
     {name: "teste", email: "teste@teste.com"},
@@ -9,5 +10,15 @@ const users = [
 export default {
     async index(req: Request, res: Response){
         return res.json(users);
+    },
+
+    async create(req: Request, res: Response){
+        const emailService = new EmailService();
+        emailService.sendMail(
+            { name: "Teste", email: "teste@teste.com" },
+            { subject: "Isso é um teste", body: "Aqui também é um teste" }
+        );
+
+        return res.send();
     }
 }
